542e077bd113db05229d10af09f18f27e4e28914
Wahab <wahababdul7297@gmail.com>
2025-04-09 13:18:18 +0500

    added a deletechannel cmd needs to be fixed tho

--- commands/utility/deleteChannel.js
+++ commands/utility/deleteChannel.js
@@ -1,18 +1,19 @@
 const {SlashCommandBuilder } = require('discord.js');
 
 
-module.exports = {
+module.exports =  {
     data: new SlashCommandBuilder()
     .setName('deletechannel')
-    .setDescription('Deletes the channel it is used in'),
+    .setDescription('Deletes the channel it is used in and sends the name of the deleter in the logs channel'),
     async execute(interaction){
         if (!interaction.memberPermissions.has('ManageChannels')){
             return;
         }   
-    const message = await interaction.channel.messages.fetch({ limit:1}).first();
-      var words = message.content.split(' ');
-      if (words[0] == `I'm`){
-        return interaction.channel.send(`Hi ${message}, I'm dad.`);
-      }
+        const guild = interaction.guild;
+        const channel = guild.channels.fetch().filter( c => c.name === `logs` && c.type === 'GUILD_TEXT');
+        const user = interaction.user;
+        
+        await interaction.channel.delete();
+        channel.send(`The ${interaction.channel.name} channel was deleted by ${user.username}`);
     }
 };
